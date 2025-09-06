1f5e278f3315b14fb101ab26ab982f74943ec634
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2025-09-07 00:18:07 +0200

    mannen ingen aning om vad som händer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import {useState} from "react";
 import {
     AreaChart,
     Area,
@@ -6,78 +6,35 @@ import {
     Line,
     XAxis,
     YAxis,
-    CartesianGrid,
     Tooltip,
-    Legend,
-    ResponsiveContainer,
 } from "recharts";
 
 import "./App.css";
 
 function App() {
     const [city, setCity] = useState<string>("");
-    const [weatherData, setWeatherData] = useState<object>({});
+    const [weatherData, setWeatherData] = useState<any>({});
 
     const apiKey: string = import.meta.env.VITE_WEATHER_API_KEY;
-
-    const apiCall = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const apiCall = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`;
 
     async function fetchData() {
         try {
             const res = await fetch(apiCall);
-            const result = await res.json();
-            setWeatherData(result);
-            console.log(result);
+            const data = await res.json();
+            setWeatherData(data);
+            console.log(data);
         } catch (error) {
             console.log(error);
         }
     }
 
-    useEffect(() => {}, []);
-
-    const data = [
-        {
-            name: "Page A",
-            uv: 4000,
-            pv: 2400,
-            amt: 2400,
-        },
-        {
-            name: "Page B",
-            uv: 3000,
-            pv: 1398,
-            amt: 2210,
-        },
-        {
-            name: "Page C",
-            uv: 2000,
-            pv: 9800,
-            amt: 2290,
-        },
-        {
-            name: "Page D",
-            uv: 2780,
-            pv: 3908,
-            amt: 2000,
-        },
-        {
-            name: "Page E",
-            uv: 1890,
-            pv: 4800,
-            amt: 2181,
-        },
-        {
-            name: "Page F",
-            uv: 2390,
-            pv: 3800,
-            amt: 2500,
-        },
-        {
-            name: "Page Hue",
-            uv: 3490,
-            pv: 4300,
-            amt: 2100,
-        },
+    let data: any = [
+        weatherData.list
+            ? weatherData.list.slice(0, 8).map((item: any, index: number) => {
+                  console.log(item);
+              })
+            : [],
     ];
 
     return (
@@ -93,61 +50,19 @@ function App() {
             <button
                 onClick={() => {
                     fetchData();
+                    console.log(data);
+
                 }}
             >
                 Get data
             </button>
 
-            <AreaChart
-                width={730}
-                height={250}
-                data={data}
-                margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-            >
-                <defs>
-                    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                        <stop
-                            offset="5%"
-                            stopColor="#8884d8"
-                            stopOpacity={0.8}
-                        />
-                        <stop
-                            offset="95%"
-                            stopColor="#8884d8"
-                            stopOpacity={0}
-                        />
-                    </linearGradient>
-                    <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-                        <stop
-                            offset="5%"
-                            stopColor="#82ca9d"
-                            stopOpacity={0.8}
-                        />
-                        <stop
-                            offset="95%"
-                            stopColor="#82ca9d"
-                            stopOpacity={0}
-                        />
-                    </linearGradient>
-                </defs>
+            <LineChart width={500} height={300} data={data}>
                 <XAxis dataKey="name" />
                 <YAxis />
-                <Tooltip />
-                <Area
-                    type="monotone"
-                    dataKey="uv"
-                    stroke="#8884d8"
-                    fillOpacity={1}
-                    fill="url(#colorUv)"
-                />
-                <Area
-                    type="monotone"
-                    dataKey="pv"
-                    stroke="#82ca9d"
-                    fillOpacity={1}
-                    fill="url(#colorPv)"
-                />
-            </AreaChart>
+                <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
+            </LineChart>
         </>
     );
 }
