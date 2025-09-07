3ccff6d0636af493a3819d53526dfcb2a6985f60
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2025-09-07 21:36:08 +0200

    now displays a chart of chosen city in a graph form

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {
     AreaChart,
     Area,
@@ -14,32 +14,34 @@ import "./App.css";
 function App() {
     const [city, setCity] = useState<string>("");
     const [weatherData, setWeatherData] = useState<any>({});
+    const [loading, setLoading] = useState<boolean>(true);
 
     const apiKey: string = import.meta.env.VITE_WEATHER_API_KEY;
     const apiCall = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`;
-
     async function fetchData() {
+        if(!city) return;
+        setLoading(true);
+
         try {
             const res = await fetch(apiCall);
             const data = await res.json();
+
             setWeatherData(data);
             console.log(data);
         } catch (error) {
             console.log(error);
+        } finally {
+            setLoading(false);
         }
     }
 
-    let data: any = [
-        weatherData.list
-            ? weatherData.list.slice(0, 8).map((item: any, index: number) => {
-                  console.log(item);
-              })
-            : [],
-    ];
+    
+
+    useEffect(() => {}, []);
 
     return (
         <>
-            <h1>Hello world</h1>
+            <h1>{weatherData.name}</h1>
             <input
                 type="text"
                 placeholder="Name city"
@@ -49,20 +51,46 @@ function App() {
             />
             <button
                 onClick={() => {
-                    fetchData();
-                    console.log(data);
-
+                    if (city) {
+                        fetchData();
+                    }
                 }}
             >
                 Get data
             </button>
 
-            <LineChart width={500} height={300} data={data}>
+  {!loading && weatherData?.list && (
+    <div>
+        <p>
+            <LineChart
+                width={500}
+                height={300}
+                data={weatherData.list.slice(0, 8).map((item: any) => ({
+                    name: item.dt_txt,
+                    pv: item.main.temp,
+                }))}
+                margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                }}
+            >
                 <XAxis dataKey="name" />
                 <YAxis />
-                <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-                <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
+                <Tooltip />
+                <Line
+                    type="monotone"
+                    dataKey="pv"
+                    stroke="#8884d8"
+                    activeDot={{r: 8}}
+                />
+                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
             </LineChart>
+        </p>
+    </div>
+  )}
+
         </>
     );
 }
